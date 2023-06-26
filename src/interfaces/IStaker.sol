6dbe2055be2448e7827b7fe94eec96f6613da576
0xtekgrinder <72015889+0xtekgrinder@users.noreply.github.com>
2023-06-26 11:10:33 +0200

    feat: update interfaces to higher than soliity 0.8.16

--- src/interfaces/IStaker.sol
+++ src/interfaces/IStaker.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.16;
+pragma solidity ^0.8.0;
 
 interface IStaker {
+  struct UserClaimedRewards {
+    address reward;
+    uint256 amount;
+  }
+
   function queueRewards(address rewardToken, uint256 amount) external returns (bool);
 
   function stake(uint256 amount, address receiver) external returns (uint256);
+
+  function unstake(uint256 amount, address receiver) external returns (uint256);
+
+  function claimRewards(address reward, address receiver) external returns (uint256);
+
+  function claimAllRewards(address receiver) external returns (UserClaimedRewards[] memory);
 }
